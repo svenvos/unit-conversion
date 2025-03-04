f34aaa1929c7e149b31625551b19492eeae78c7e
Sven Vos <161502162+svenvos@users.noreply.github.com>
2025-03-04 10:18:01 +0100

    went from 3 functions for converting to 1, by accepting more arguments inside the 1 function

--- index.js
+++ index.js
@@ -6,28 +6,37 @@ const massEl = document.getElementById("mass-el");
 
 convertBtn.addEventListener("click", () => {
     let numberToConvertValue = Number(inputToConvert.value);
-    convertLength(numberToConvertValue);
-    convertVolume(numberToConvertValue);
-    convertMass(numberToConvertValue);
+    length = convert(numberToConvertValue, "meters", "feet", 3.281);
+    lengthEl.textContent = length;
+    volume = convert(numberToConvertValue, "liters", "gallons", 0.264);
+    volumeEl.textContent = volume;
+    mass = convert(numberToConvertValue, "kilos", "pounds", 2.204);
+    massEl.textContent = mass;
 });
 
-function convertLength(length) {
-    const meterToFeet = (length * 3.281).toFixed(3);
-    const feetToMeter = (length / 3.281).toFixed(3);
-    let templateString = `${length} meters = ${meterToFeet} feet | ${length} feet = ${feetToMeter} meters`;
-    lengthEl.textContent = templateString;
+function convert(type, numberToConvert, metric, imperial, conversion) {
+    let valueImperial = (numberToConvert * conversion).toFixed(3);
+    let valueMetric = (numberToConvert / conversion).toFixed(3);
+    return templateString = `${numberToConvert} ${metric} = ${valueImperial} ${imperial} | ${numberToConvert} ${imperial} = ${valueMetric} ${metric}`;
 }
 
-function convertVolume(volume) {
-    const litersToGallons = (volume * 0.264).toFixed(3);
-    const gallonsToLiters = (volume / 0.264).toFixed(3);
-    let templateString = `${volume} liters = ${litersToGallons} gallons | ${volume} gallons = ${gallonsToLiters} liters`;
-    volumeEl.textContent = templateString;
-}
+// function convertLength(length) {
+//     const meterToFeet = (length * 3.281).toFixed(3);
+//     const feetToMeter = (length / 3.281).toFixed(3);
+//     let templateString = `${length} meters = ${meterToFeet} feet | ${length} feet = ${feetToMeter} meters`;
+//     lengthEl.textContent = templateString;
+// }
 
-function convertMass(mass) {
-    const kilosToPounds = (mass * 2.204).toFixed(3);
-    const poundsToKilos = (mass / 2.204).toFixed(3);
-    let templateString = `${mass} kilos = ${kilosToPounds} pounds | ${mass} pounds = ${poundsToKilos} kilos`;
-    massEl.textContent = templateString;
-}
+// function convertVolume(volume) {
+//     const litersToGallons = (volume * 0.264).toFixed(3);
+//     const gallonsToLiters = (volume / 0.264).toFixed(3);
+//     let templateString = `${volume} liters = ${litersToGallons} gallons | ${volume} gallons = ${gallonsToLiters} liters`;
+//     volumeEl.textContent = templateString;
+// }
+
+// function convertMass(mass) {
+//     const kilosToPounds = (mass * 2.204).toFixed(3);
+//     const poundsToKilos = (mass / 2.204).toFixed(3);
+//     let templateString = `${mass} kilos = ${kilosToPounds} pounds | ${mass} pounds = ${poundsToKilos} kilos`;
+//     massEl.textContent = templateString;
+// }
